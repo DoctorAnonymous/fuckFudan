2a4a1a3f5e08cb252393f67be3622736df3224a6
DoctorAnonymous <zcmscy@icloud.com>
2021-11-14 00:15:11 +0800

    Update fuckFudan.py

--- fuckFudan.py
+++ fuckFudan.py
@@ -1,18 +1,22 @@
 from selenium import webdriver
 import time
 import ddddocr
-from PIL import Image,ImageEnhance
+from PIL import Image, ImageEnhance
+import requests
+
 
 def fuckFudan(username, password):
     try:
-        time.sleep(2)
+
         browser = webdriver.Chrome()
         browser.get('https://zlapp.fudan.edu.cn/site/ncov/fudanIndex')
+        if 'undefined' in browser.page_source:
+            return('Error')
         time.sleep(2)
         # 登录
         browser.find_element_by_id('username').send_keys(username)
         browser.find_element_by_id('password').send_keys(password)
-        browser.find_element_by_id('idcheckloginbtn').click()        
+        browser.find_element_by_id('idcheckloginbtn').click()
     except:
         pass
     try:
@@ -22,6 +26,7 @@ def fuckFudan(username, password):
         browser.find_element_by_css_selector('.wapat-btn.wapat-btn-ok').click()
     except:
         browser.close()
+        time.sleep(600)
         return(username)
         # 是否在校
     try:
@@ -40,19 +45,19 @@ def fuckFudan(username, password):
         time.sleep(2)
         browser.find_element_by_partial_link_text('提交').click()
         time.sleep(2)
-        browser.find_element_by_css_selector('.wapcf-btn.wapcf-btn-ok').click() 
-        # 验证码   
+        browser.find_element_by_css_selector('.wapcf-btn.wapcf-btn-ok').click()
+        # 验证码
         time.sleep(2)
         browser.save_screenshot('test.png')
-        Image.open("test.png").crop((397,363,512,400)).save('test.png')
-        with open('test.png','rb') as inputFile:
-            code=ddddocr.DdddOcr().classification(inputFile.read())
+        Image.open("test.png").crop((397, 363, 512, 400)).save('test.png')
+        with open('test.png', 'rb') as inputFile:
+            code = ddddocr.DdddOcr().classification(inputFile.read())
         browser.find_element_by_class_name('wapat-title-input').find_element_by_tag_name('input').send_keys(code)
         browser.find_element_by_css_selector('.wapat-btn.wapat-btn-ok').click()
         time.sleep(3)
     except Exception as e:
         pass
     browser.close()
+    time.sleep(0)
     return(fuckFudan(username, password))
 
-# fuckFudan('username','password')
